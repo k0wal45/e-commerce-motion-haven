eaa3bb882aee576f1a366fdad7bfacebf81e62ee
k0wal45 <d.kowal.com@gmail.com>
2023-12-26 13:30:39 +0100

    routing update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import { BrowserRouter as Router,  Routes, Route } from "react-router-dom"; 
 import Home from "./pages/Home";
+import Category from "./pages/Category";
+import Cart from "./pages/Cart";
+import Admin from "./pages/Admin";
+import Checkout from "./pages/Checkout";
+import Product from "./pages/Product";
+import Notfound from "./pages/Notfound";
 
 
 function App() {
@@ -8,6 +14,13 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/category/:catId" element={<Category/>}/>
+        <Route path="/product/:prodId" element={<Product/>}/>
+        <Route path="/cart" element={<Cart/>}/>
+        <Route path="/admin" element={<Admin/>}/>
+        <Route path="/checkout" element={<Checkout/>}/>
+        <Route path="/notfound" element={<Notfound/>}/>
+        <Route path="/*" element={<Notfound/>}/>
       </Routes>
     </Router>
   )
