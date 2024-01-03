27aa63229e94aaaf0d7f1f2674eb383e66fae024
k0wal45 <d.kowal.com@gmail.com>
2024-01-03 12:54:16 +0100

    circle fix

--- src/components/Decoration.jsx
+++ src/components/Decoration.jsx
@@ -28,23 +28,23 @@ const Decoration = () => {
 
       <ul className="absolute flex items-center flex-nowrap text-7xl lg:text-[10rem] gap-16 font-luckycoin p-8 top-4 animate-move z-20" >
         <li>Comfortable</li>
-        <li className="p-8 lg:p-20 bg-base-100 rounded-full aspect-square"></li>
+        <li className="p-8 lg:p-20 bg-white rounded-full aspect-square"></li>
         <li>Playful</li>
-        <li className="p-8 lg:p-20 bg-base-100 rounded-full aspect-square"></li>
+        <li className="p-8 lg:p-20 bg-white rounded-full aspect-square"></li>
         <li>Durable</li>
-        <li className="p-8 lg:p-20 bg-base-100 rounded-full aspect-square"></li>
+        <li className="p-8 lg:p-20 bg-white rounded-full aspect-square"></li>
         <li>Colorful</li>
-        <li className="p-8 lg:p-20 bg-base-100 rounded-full aspect-square"></li>
+        <li className="p-8 lg:p-20 bg-white rounded-full aspect-square"></li>
         <li>Adorable</li>
-        <li className="p-8 lg:p-20 bg-base-100 rounded-full aspect-square"></li>
+        <li className="p-8 lg:p-20 bg-white rounded-full aspect-square"></li>
         <li>Practical</li>
-        <li className="p-8 lg:p-20 bg-base-100 rounded-full aspect-square"></li>
+        <li className="p-8 lg:p-20 bg-white rounded-full aspect-square"></li>
         <li>Versatile</li>
-        <li className="p-8 lg:p-20 bg-base-100 rounded-full aspect-square"></li>
+        <li className="p-8 lg:p-20 bg-white rounded-full aspect-square"></li>
         <li>Stylish</li>
-        <li className="p-8 lg:p-20 bg-base-100 rounded-full aspect-square"></li>
+        <li className="p-8 lg:p-20 bg-white rounded-full aspect-square"></li>
         <li>Soft</li>
-        <li className="p-8 lg:p-20 bg-base-100 rounded-full aspect-square"></li>
+        <li className="p-8 lg:p-20 bg-white rounded-full aspect-square"></li>
         <li>Safe</li>
       </ul>
       <ul className="absolute flex items-center flex-nowrap text-7xl lg:text-[10rem] gap-16 font-luckycoin p-8 top-8 animate-move z-10 text-red-200" >
@@ -72,23 +72,23 @@ const Decoration = () => {
 
       <ul className="flex justify-around items-center overflow-hidden flex-nowrap text-xl w-full gap-4 ">
         <li>Comfortable</li>
-        <li className="p-2 bg-base-100 rounded-full"></li>
+        <li className="p-2 bg-white rounded-full"></li>
         <li>Playful</li>
-        <li className="p-2 bg-base-100 rounded-full"></li>
+        <li className="p-2 bg-white rounded-full"></li>
         <li>Durable</li>
-        <li className="p-2 bg-base-100 rounded-full"></li>
+        <li className="p-2 bg-white rounded-full"></li>
         <li>Colorful</li>
-        <li className="p-2 bg-base-100 rounded-full"></li>
+        <li className="p-2 bg-white rounded-full"></li>
         <li>Adorable</li>
-        <li className="p-2 bg-base-100 rounded-full"></li>
+        <li className="p-2 bg-white rounded-full"></li>
         <li>Practical</li>
-        <li className="p-2 bg-base-100 rounded-full"></li>
+        <li className="p-2 bg-white rounded-full"></li>
         <li>Versatile</li>
-        <li className="p-2 bg-base-100 rounded-full"></li>
+        <li className="p-2 bg-white rounded-full"></li>
         <li>Stylish</li>
-        <li className="p-2 bg-base-100 rounded-full"></li>
+        <li className="p-2 bg-white rounded-full"></li>
         <li>Soft</li>
-        <li className="p-2 bg-base-100 rounded-full"></li>
+        <li className="p-2 bg-white rounded-full"></li>
         <li>Safe</li>
       </ul>
     </section>
